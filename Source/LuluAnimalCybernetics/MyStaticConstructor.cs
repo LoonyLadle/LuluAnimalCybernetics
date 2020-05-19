f695c014b6cc7d3c43aa2d964984f8f7b735af80
LoonyLadle <LoonyLadle@users.noreply.github.com>
2020-05-19 06:43:48 -0400

    patcher working now

--- Source/LuluAnimalCybernetics/MyStaticConstructor.cs
+++ Source/LuluAnimalCybernetics/MyStaticConstructor.cs
@@ -14,15 +14,29 @@ namespace LoonyLadle.AnimalCybernetics
 		static MyStaticConstructor()
 		{
 			IEnumerable<RecipeDef> surgeries = DefDatabase<RecipeDef>.AllDefs.Where(r => r.IsSurgery);
-			IEnumerable<ThingDef> animals = DefDatabase<ThingDef>.AllDefs.Where(t => t.race.Animal);
+			IEnumerable<ThingDef> animals = DefDatabase<ThingDef>.AllDefs.Where(t => t.race?.Animal ?? false);
+			StringBuilder stringBuilder = new StringBuilder("[LuluAnimalCybernetics] Dynamic patched the following defs: ");
+			bool first = true;
 
 			foreach (RecipeDef surgery in surgeries)
 			{
 				if (surgery.AllRecipeUsers.Contains(ThingDefOf.Human) && !surgery.AllRecipeUsers.Contains(ThingDefOf.Dromedary))
 				{
 					surgery.recipeUsers.AddRange(animals);
+
+					if (first)
+					{
+						stringBuilder.Append(surgery.defName);
+						first = false;
+					}
+					else
+					{
+						stringBuilder.AppendWithComma(surgery.defName);
+					}
+
 				}
 			}
+			Log.Message(stringBuilder.ToString());
 		}
 	}
 }

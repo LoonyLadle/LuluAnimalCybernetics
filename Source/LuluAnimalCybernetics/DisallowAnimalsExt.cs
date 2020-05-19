c1e2686859ac9efec2a2b440fc35b1ab11fb48fb
Lulu <48167745+LoonyLadle@users.noreply.github.com>
2020-05-19 09:55:08 -0400

    2.0.0
    * Updated for RimWorld version 1.1 and the Royalty DLC
    * Possibility of insects being good boys begrudgingly acknowledged
    * Radical redesign using the infinite power of C# modding
    * Eliminated mandatory compatibility patches for every mod!
    * All surgeries available to all animals, full stop
    * Surgeries can be purposefully excluded from animals with a new ModExtension (see patch file for how this was done for core recipes)

---
+++ Source/LuluAnimalCybernetics/DisallowAnimalsExt.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace LoonyLadle.AnimalCybernetics
+{
+	public class DisallowAnimalsExt : DefModExtension
+	{
+		// This extension does not contain any data, it just needs to exist.
+	}
+}
---
+++ Source/LuluAnimalCybernetics/MyStaticConstructor.cs
@@ -0,0 +1,46 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Verse;
+
+namespace LoonyLadle.AnimalCybernetics
+{
+	[StaticConstructorOnStartup]
+	public static class MyStaticConstructor
+	{
+		static MyStaticConstructor()
+		{
+			IEnumerable<RecipeDef> surgeries = DefDatabase<RecipeDef>.AllDefs.Where(r => r.IsSurgery);
+			IEnumerable<ThingDef> animals = DefDatabase<ThingDef>.AllDefs.Where(t => t.race?.Animal ?? false);
+			StringBuilder stringBuilder = new StringBuilder("[LuluAnimalCybernetics] Dynamic patched the following defs: ");
+			bool first = true;
+
+			foreach (RecipeDef surgery in surgeries)
+			{
+				if (!surgery.HasModExtension<DisallowAnimalsExt>() && surgery.AllRecipeUsers.Contains(ThingDefOf.Human) && !surgery.AllRecipeUsers.Contains(ThingDefOf.Muffalo))
+				{
+					surgery.recipeUsers.AddRange(animals);
+
+					if (first)
+					{
+						stringBuilder.Append(surgery.defName);
+						first = false;
+					}
+					else
+					{
+						stringBuilder.AppendWithComma(surgery.defName);
+					}
+
+				}
+			}
+			foreach (ThingDef animal in animals)
+			{
+				// Clear recipe cache; shouldn't be necessary unless other mods have forced it to be created somehow.
+				typeof(ThingDef).GetField("allRecipesCached", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(animal, null);
+			}
+			Log.Message(stringBuilder.ToString());
+		}
+	}
+}

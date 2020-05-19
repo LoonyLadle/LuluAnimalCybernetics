cd9fb2bec0fac5ad9151b34d4980a5f11580d05a
LoonyLadle <LoonyLadle@users.noreply.github.com>
2020-05-19 05:15:22 -0400

    added OMNI-PATCH
    The days of needing a seperate patch for every single bionics mod are over!

---
+++ Source/LuluAnimalCybernetics/MyStaticConstructor.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace LuluAnimalCybernetics
+{
+	[StaticConstructorOnStartup]
+	public static class MyStaticConstructor
+	{
+		static MyStaticConstructor()
+		{
+			IEnumerable<RecipeDef> surgeries = DefDatabase<RecipeDef>.AllDefs.Where(r => r.IsSurgery);
+			IEnumerable<ThingDef> animals = DefDatabase<ThingDef>.AllDefs.Where(t => t.race.Animal);
+
+			foreach (RecipeDef surgery in surgeries)
+			{
+				if (surgery.AllRecipeUsers.Contains(ThingDefOf.Human) && !surgery.AllRecipeUsers.Contains(ThingDefOf.Dromedary))
+				{
+					surgery.recipeUsers.AddRange(animals);
+				}
+			}
+		}
+	}
+}
